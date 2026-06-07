3d06cdae770b8498b29cb690c3e72ee1d7cc153c
adolfoxd3623 <adolfomeneses49@gmail.com>
2026-06-07 03:46:33 -0500

    Update AdmiNivelacion.py

--- AdmiNivelacion.py
+++ AdmiNivelacion.py
@@ -1,3 +1,5 @@
+from Usuario import Usuario
 #Inicio clase Administrador
-class Administrador:
-  
+class Administrador(Usuario):
+    def __init__(self, idUsuario, nombre, correo, contrasena, rol,
+                 sede, areaResponsable, telefono):
