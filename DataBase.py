2438c8d3b67634a0979888c8b6b7d2e71086d87d
ByJeff <jeffersonmoreiram9@gmail.com>
2026-06-20 21:11:13 -0500

    Implementacion de SQL en el sistema

---
+++ DataBase.py
@@ -0,0 +1,75 @@
+import pyodbc #--> en caso de que suelte error es por que la libreria no se encuentra descargada, use pip install pyodbc en su terminal.
+
+class ConexionSQLServer:
+    def __init__(self):
+      #se conecta a un SQL local
+        self.server = 'localhost\\SQLEXPRESS'
+        self.database= 'SistemaDeNivelacion'
+
+        self.conexion = None
+        self.cursor = None
+
+  #Crea la base de datos en caso de no existir
+    def crearBaseDedatos(self):
+        try:
+            conexion = pyodbc.connect(
+                f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+                f"SERVER={self.server};"
+                f"Trusted_Connection=yes;"
+            )
+
+          #interprete entre python y SQL
+            cursor = conexion.cursor()
+
+            cursor.execute(f"""
+                IF NOT EXISTS (
+                    SELECT name FROM sys.databases 
+                    WHERE name = '{self.database}'
+                )
+                CREATE DATABASE {self.database}
+            """)
+
+            conexion.commit()
+            cursor.close()
+            conexion.close()
+
+            print("Base de datos verificada/creada")
+        except Exception as e:
+            print(f"Error creando BD: {e}")
+            
+
+
+     #Conexion con la base de datos   
+    def conectar(self):
+        try:
+            self.connection_string = (
+            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+            f"SERVER={self.server};"
+            f"DATABASE={self.database};"
+            f"Trusted_Connection=yes;"
+            )
+
+            self.cursor = self.conexion.cursor()
+
+            print("Conexion exitosa con SQL Server")
+            return self.conexion
+        
+        except Exception as e:
+            print(f"Error al conectar: {e}")
+            return None
+
+
+    #Desconexion de la base de datos    
+    def cerrarConexion(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.conexion:
+            self.conexion.close()
+            print("Conexion cerrada")
+
+
+if __name__ == "__main__":
+    db = ConexionSQLServer()
+    db.crearBaseDedatos()
+    db.conectar()
+    db.cerrarConexion()
