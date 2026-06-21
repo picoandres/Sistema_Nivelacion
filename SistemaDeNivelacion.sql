388eb00d8447a6bdd2aaeeb19dcaa8d003c29ad0
ByJeff <jeffersonmoreiram9@gmail.com>
2026-06-20 21:16:46 -0500

    Tablas de SQL para el sistema

---
+++ SistemaDeNivelacion.sql
@@ -0,0 +1,43 @@
+--Tabla Padre
+CREATE TABLE Usuario(
+	cedula VARCHAR(20),
+	nombre VARCHAR(80) NOT NULL,
+	correo VARCHAR(100) UNIQUE NOT NULL,
+	contrasena VARCHAR(255) NOT NULL,
+	rol VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_cedula PRIMARY KEY (cedula)
+);
+
+-- Tablas Hijas
+CREATE TABLE Alumnos(
+	cedula VARCHAR(20),
+	carrera VARCHAR(50),
+	paralelo VARCHAR(10),
+	CONSTRAINT PK_Estudiante_cedula PRIMARY KEY (cedula),
+	CONSTRAINT FK_Estudiante_usuario 
+	FOREIGN KEY (cedula) REFERENCES Usuario(cedula) ON DELETE CASCADE
+);
+
+CREATE TABLE Docente(
+	cedula VARCHAR(20),
+	titulo VARCHAR(50),
+	especialidad VARCHAR(50),
+	paralelo VARCHAR(10),
+	CONSTRAINT PK_Docente_cedula PRIMARY KEY(cedula),
+	CONSTRAINT FK_Docente_usuario 
+	FOREIGN KEY (cedula) REFERENCES Usuario(cedula) ON DELETE CASCADE
+);
+
+CREATE TABLE Administrador(
+	cedula VARCHAR(20),
+	id_admin INT UNIQUE,
+	sede VARCHAR(50),
+	telefono VARCHAR(20),
+	CONSTRAINT PK_Administrador_cedula PRIMARY KEY (cedula),
+	CONSTRAINT FK_Administrador_usuario 
+	FOREIGN KEY (cedula) REFERENCES Usuario(cedula) ON DELETE CASCADE
+);
+
+SELECT * FROM Usuario;
+SELECT * FROM Alumnos;
+SELECT * FROM Docente;
