d8c837e24d841f51dd77a86b5c7f38788ba6545c
picoandres <janlcaxpl@gmail.com>
2026-06-28 21:05:15 -0500

    Clase innecesaria eliminada

--- Notificacion.py
+++
@@ -1,8 +0,0 @@
-from GestorNotificaciones import gestorNotificaciones
-class Notificacion(gestorNotificaciones):
-    def __init__(self):
-        super().__init__()
-
-    def enviar(self, mensaje):
-        print("enviando notificaciones...")
-        self.notificar(mensaje)
