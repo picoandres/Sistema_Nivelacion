7b11b37ecf803e7e866966cb32cebb1f842f2439
picoandres <janlcaxpl@gmail.com>
2026-07-07 19:49:29 -0500

    Nueva clase

---
+++ Evaluacion.py
@@ -0,0 +1,9 @@
+class Evaluacion:
+    def __init__(self, idEvaluacion, idCurso, idMateria, titulo, descripcion, fecha, ponderacion):
+        self.idEvaluacion = idEvaluacion
+        self.idCurso = idCurso
+        self.idMateria = idMateria
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.fecha = fecha
+        self.ponderacion = ponderacion
