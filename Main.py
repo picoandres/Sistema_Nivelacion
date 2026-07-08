59b45603347999fe64cc6d41e999e8419a207aad
picoandres <janlcaxpl@gmail.com>
2026-07-07 20:17:32 -0500

    Menús mucho más limpios, todas las opciones de los menús implementadas

--- Main.py
+++ Main.py
@@ -1,104 +1,39 @@
-from CursoNivelacion import CursoNivelacion
-from Estudiante import Estudiante
-from HorarioFactory import obtenerFabricaHorario
-from Docente import Docente
-from SistemaDAO import UsuarioDAO, EstudianteDAO, DocenteDAO, AdministradorDAO, CursoDAO
+from UsuarioDAO import UsuarioDAO
+from EstudianteDAO import EstudianteDAO
+from DocenteDAO import DocenteDAO
+from AdministradorDAO import AdministradorDAO
+from CursoDAO import CursoDAO
+from AsignacionCursoDAO import AsignacionCursoDAO
+from CalificacionDAO import CalificacionDAO
+from MateriaDAO import MateriaDAO
+from CursoMateriaDAO import CursoMateriaDAO
+from EvaluacionDAO import EvaluacionDAO
+from HorarioDAO import HorarioDAO
 from GestorNotificaciones import GestorNotificaciones
-
-#inicializacion de DAOs:
-usuario_dao = UsuarioDAO()
-estudiante_dao = EstudianteDAO()
-docente_dao = DocenteDAO()
-administrador_dao = AdministradorDAO()
-curso_dao = CursoDAO()
-
-#Patrón de comportamiento Observer:
-gestor = GestorNotificaciones()
+from GestorAulas import GestorAulas
 
 class Sistema():
     def __init__(self):
         self.usuario_actual = None
+        self.usuario_dao = UsuarioDAO()
+        self.estudiante_dao = EstudianteDAO()
+        self.docente_dao = DocenteDAO()
+        self.administrador_dao = AdministradorDAO()
+        self.curso_dao = CursoDAO()
+        self.materia_dao = MateriaDAO()
+        self.asignacionCurso_dao = AsignacionCursoDAO()
+        self.cursoMateria_dao = CursoMateriaDAO()
+        self.calificacion_dao = CalificacionDAO()
+        self.evaluacion_dao = EvaluacionDAO()
+        self.horario_dao = HorarioDAO()
+        self.gestor = GestorNotificaciones()
+        self.gestor_aulas = GestorAulas(self.horario_dao)
 
     def sistemaNivelacion(self):
         while True:
             print("----------SISTEMA NIVELACION----------")
-            print("1. Registrar Estudiante")
-            print("2. Registrar Docente")
-            print("3. Iniciar sesión")
-            print("4. Salir")
-
-            opcion = input("Escoja una opción: ")
-
-            if opcion == "1":
-                cedula = input("Cédula: ")
-                nombre = input("Nombre: ")
-                correo = input("Correo: ")
-                contrasena = input("Contraseña: ")
-                carrera = input("Carrera: ")
-                paralelo = input("Paralelo: ")
-                
-                estudiante = Estudiante(
-                    cedula,
-                    nombre,
-                    correo,
-                    contrasena,
-                    "Estudiante",
-                    carrera,
-                    paralelo
-                    )
-
-                if estudiante_dao.guardar(estudiante):
-                    gestor.agregar_observador(estudiante)
-                    print("Estudiante guardado en BD correctamente\n")
-                    gestor.notificar_Todos("Se registró un nuevo estudiante")
-                    gestor.eliminar_observador(estudiante)
-                else:
-                    print("Error al guardar estudiante en BD\n")
-
-            elif opcion == "2":
-                cedula = input("Cédula: ")
-                nombre = input("Nombre: ")
-                correo = input("Correo: ")
-                contrasena = input("Contraseña: ")
-                titulo = input("Título Académico: ")
-                especialidad = input("Especialidad: ")
-            
-                docente = Docente(
-                    cedula,
-                    nombre,
-                    correo,
-                    contrasena,
-                    "Docente",
-                    titulo,
-                    especialidad
-                )
-
-                if docente_dao.guardar(docente):
-                    gestor.agregar_observador(docente)
-                    print("Docente guardado en BD correctamente\n")
-                    gestor.notificar_Todos("Se registró un nuevo Docente")
-                    gestor.eliminar_observador(docente)
-                else:
-                    print("Error al guardar docente en BD\n")
-
-            elif opcion == "3":
-                sistema.iniciarSesion()
-
-            elif opcion == "4":
-                print("Tenga buen dia")
-                break
-            else:
-                print("Opción inválida, intente de nuevo\n")
-        
-
-    def iniciarSesion(self):
-        while True:
-            print()
-            print("----------INICIO DE SESIÓN----------")
             print("1. Iniciar sesión")
-            print("2. Recuperar contraseña")
-            print("3. Regresar a menú principal")
-            print()
+            print("2. Salir")
 
             opcion = input("Escoja una opción: ")
 
@@ -106,7 +41,7 @@ class Sistema():
                 correo = input("Correo: ")
                 contrasena = input("Contraseña: ")
                 
-                usuario = usuario_dao.buscarUsuario(correo, contrasena)
+                usuario = self.usuario_dao.buscarUsuario(correo, contrasena)
 
                 if usuario:
                     print(f"\nBienvenido {usuario.nombre}")
@@ -116,12 +51,11 @@ class Sistema():
                 else:
                     print("Correo o contraseña incorrectos")
 
+    
             elif opcion == "2":
-                pass
+                print("Tenga buen dia")
+                break
 
-            elif opcion == "3":
-                return            
-            
             else:
                 print("Opción inválida, intente de nuevo\n")
 
@@ -141,141 +75,147 @@ class Sistema():
         while True:
             print("\n===== MENÚ ADMINISTRADOR =====")
             print("1. Ver perfil")
-            print("2. Crear curso")
-            print("3. Asignar docente a curso")
-            print("4. Listar estudiantes")
-            print("5. Listar docentes")
-            print("6. Listar cursos")
-            print("7. Ver historial")
-            print("8. Salir")
+            print("2. Cambiar contraseña")
+            print("3. Crear curso")
+            print("4. Registrar materia")
+            print("5. Asignar docente a curso")
+            print("6. Asignar estudiante a curso")
+            print("7. Asignar materia a curso")
+            print("8. Listar estudiantes")
+            print("9. Listar docentes")
+            print("10. Listar cursos")
+            print("11. Ver horario")
+            print("12. Ver historial")
+            print("13. Cerrar sesión")
 
-            opcion = input("Escoja una opción: ")
+            opcion = input("Escoja una opción: ").strip()
 
             if opcion == "1":
                 self.usuario_actual.verPerfil()
 
             elif opcion == "2":
-                idCurso = input("ID del curso: ")
-                nombreCurso = input("Nombre del curso: ")
-                modalidad = input("Modalidad: ")
-                jornada = input("Jornada: ")
-                dia = input("Dia: ")
-                aula = input("Aula: ")
+                self.usuario_actual.cambiarContrasena()
 
-                try:
-                    fabrica = obtenerFabricaHorario(jornada)
-                    horario = fabrica.crearHorario(
-                        dia,
-                        aula,
-                        self.usuario_actual.nombre
-                    )
-
-                    curso = CursoNivelacion(
-                        idCurso,
-                        nombreCurso,
-                        modalidad,
-                        jornada,
-                        horario
-                    )
-
-                    self.usuario_actual.registrarAccion(f"Se creó el curso: {nombreCurso}")
-                    if curso_dao.guardar(curso):
-                        print("Curso creado exitosamente")
-                    else:
-                        print("No se pudo crear el curso")
-
-                except Exception as e:
-                    print("Error al crear el curso: ", e)
-            
             elif opcion == "3":
-                print("-----ASIGNAR DOCENTE-----")
-                idCurso = input("ID del Curso: ")
-                cedulaDocente = input("Cédula del docente: ")
-                curso_dao.asignarDocente(idCurso, cedulaDocente)
-                print("Docente asignado")
+                self.usuario_actual.crearCurso(self.curso_dao, self.horario_dao, self.gestor_aulas)
 
             elif opcion == "4":
-                estudiantes = estudiante_dao.listar()
-                print("\n----- ESTUDIANTES -----")
-                for e in estudiantes:
-                    print(f"{e.nombre} - {e.carrera} - {e.paralelo}")
+                self.usuario_actual.registrarMateria(self.materia_dao)
 
             elif opcion == "5":
-                docentes = docente_dao.listar()
-                print("\n----- DOCENTES -----")
-                for d in docentes:
-                    print(f"{d.nombre} - {d.titulo}")
+                self.usuario_actual.asignarDocenteACurso(self.curso_dao,self.docente_dao, self.cursoMateria_dao)
 
             elif opcion == "6":
-                cursos = curso_dao.listar()
-                self.usuario_actual.mostrarCursos(cursos)
+                self.usuario_actual.asignarEstudianteACurso(self.curso_dao,self.estudiante_dao,self.asignacionCurso_dao)
 
             elif opcion == "7":
-                pass
+                self.usuario_actual.asignarMateriaACurso(self.curso_dao,self.materia_dao,self.cursoMateria_dao)
 
             elif opcion == "8":
+                self.usuario_actual.listarEstudiantes(self.estudiante_dao)
+
+            elif opcion == "9":
+                self.usuario_actual.listarDocentes(self.docente_dao)
+
+            elif opcion == "10":
+                self.usuario_actual.listarCursos(self.curso_dao)
+
+            elif opcion == "11":
+                self.usuario_actual.verHorario(self.horario_dao)
+
+            elif opcion == "12":
+                self.usuario_actual.mostrarHistorial()
+
+            elif opcion == "13":
                 print("Cerrando sesión\n")
+                self.usuario_actual = None
                 break
 
             else:
-                print("Opción inválida, intente de nuevo")
+                print("\nOpción inválida, intente de nuevo\n")
 
 
     def menuDocente(self):
         while True:
             print("\n===== MENÚ DOCENTE =====")
             print("1. Ver perfil")
-            print("2. Ver cursos asignados")
-            print("3. Ver Materias Disponibles")
-            print("4. Ver estudiantes")
-            print("5. Calificar estudiante")
-            print("6. Crear evaluación")
-            print("7. Ver cronograma")
-            print("8. Cerrar sesión")
+            print("2. Cambiar contraseña")
+            print("3. Ver cursos asignados")
+            print("4. Ver mi materia")
+            print("5. Ver estudiantes")
+            print("6. Calificar estudiante")
+            print("7. Ver calificaciones")
+            print("8. Editar calificación")
+            print("9. Eliminar calificación")
+            print("10. Crear evaluación")
+            print("11. Ver evaluaciones")
+            print("12. Editar evaluación")
+            print("13. Eliminar evaluación")
+            print("14. Ver horario")
+            print("15. Cerrar sesión")
 
             opcion = input("Escoja una opción: ")
 
             if opcion == "1":
-                self.usuario_actual.verPerfil()
+                self.usuario_actual.verPerfil(self.docente_dao)
 
             elif opcion == "2":
-                cursos = curso_dao.buscarPorDocente(self.usuario_actual.cedula)
-                self.usuario_actual.mostrarCursosAsignados(cursos)
+                self.usuario_actual.cambiarContrasena()
 
             elif opcion == "3":
-                pass
+                self.usuario_actual.verCursosAsignados(self.curso_dao)
 
             elif opcion == "4":
-                pass
+                self.usuario_actual.verMateria(self.docente_dao)
 
             elif opcion == "5":
-                pass
+                self.usuario_actual.verEstudiantes(self.curso_dao, self.asignacionCurso_dao)
 
             elif opcion == "6":
-                titulo_evaluacion = input("Título de evaluación: ")
-                descripcion = input("Descripción: ")
-                self.usuario_actual.crearEvaluacion(titulo_evaluacion, descripcion)
+                self.usuario_actual.calificarEstudiante(self.curso_dao, self.asignacionCurso_dao, self.evaluacion_dao, self.calificacion_dao)
 
             elif opcion == "7":
-                pass
+                self.usuario_actual.verCalificaciones(self.curso_dao, self.calificacion_dao)
 
             elif opcion == "8":
+                self.usuario_actual.editarCalificacion(self.curso_dao, self.calificacion_dao)
+
+            elif opcion == "9":
+                self.usuario_actual.eliminarCalificacion(self.curso_dao, self.calificacion_dao)
+
+            elif opcion == "10":
+                self.usuario_actual.crearEvaluacion(self.curso_dao, self.materia_dao, self.evaluacion_dao, self.asignacionCurso_dao)
+
+            elif opcion == "11":
+                self.usuario_actual.verEvaluaciones(self.evaluacion_dao)
+
+            elif opcion == "12":
+                self.usuario_actual.editarEvaluacion(self.evaluacion_dao)
+            
+            elif opcion == "13":
+                self.usuario_actual.eliminarEvaluacion(self.evaluacion_dao)
+
+            elif opcion == "14":
+                self.usuario_actual.verHorario(self.horario_dao)
+
+            elif opcion == "15":
                 print("Cerrando sesión\n")
+                self.usuario_actual = None
                 break
 
             else:
-                print("Opción inválida, intente de nuevo")
+                print("\nOpción inválida, intente de nuevo\n")
+
 
     def menuEstudiante(self):
         while True:
             print("\n===== MENÚ ESTUDIANTE =====")
             print("1. Ver perfil")
-            print("2. Ver notas")
-            print("3. Ver asistencia")
-            print("4. Subir documento")
-            print("5. Ver documentos")
-            print("6. Cambiar contraseña")
-            print("7. Cerrar sesión")
+            print("2. Cambiar contraseña")
+            print("3. Mis cursos")
+            print("4. Ver notas")
+            print("5. Ver horario")
+            print("6. Cerrar sesión")
 
             opcion = input("Escoja una opción: ")
 
@@ -283,27 +223,24 @@ class Sistema():
                 self.usuario_actual.verPerfil()
 
             elif opcion == "2":
-                self.usuario_actual.verNotas()
+                self.usuario_actual.cambiarContrasena(self.usuario_dao)
 
             elif opcion == "3":
-                pass
-                
+                self.usuario_actual.verCursos(self.asignacionCurso_dao)
+
             elif opcion == "4":
-                nombre = input("Nombre del documento: ")
-                tipo = input("Tipo de documento: ")
-                self.usuario_actual.subirDocumentos(nombre, tipo)
+                self.usuario_actual.verNotas(self.calificacion_dao)
 
             elif opcion == "5":
-                self.usuario_actual.verDocumentosSubidos()
+                self.usuario_actual.verHorario(self.horario_dao)
 
             elif opcion == "6":
-                pass
-
-            elif opcion == "7":
                 print("Cerrando sesión\n")
+                self.usuario_actual = None
                 break
+            
             else:
-                print("Opción inválida, intente de nuevo")
+                print("\nOpción inválida, intente de nuevo\n")
 
 sistema = Sistema()
 sistema.sistemaNivelacion()
