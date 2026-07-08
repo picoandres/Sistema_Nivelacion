4846da32e94e52980e57b3328d8213c46e39116e
picoandres <janlcaxpl@gmail.com>
2026-07-07 19:46:16 -0500

    Se quitaron métodos viejos que no se usaban

--- CursoNivelacion.py
+++ CursoNivelacion.py
@@ -1,6 +1,3 @@
-#Relaciones con otras clases
-from Docente import Docente
-
 class CursoNivelacion:
     def __init__(self, idCurso, nombreCurso, modalidad, jornada, horario, docente = None):
         self.idCurso = idCurso
@@ -8,58 +5,10 @@ class CursoNivelacion:
         self.modalidad = modalidad
         self.jornada = jornada
         self.horario = horario
-
         self.docente = docente
-        self.estudiantes = []
-        self.historial = []
     
     def mostrarInformacion(self):
         print(f"ID: {self.idCurso}")
         print(f"Nombre: {self.nombreCurso}")
         print(f"Modalidad: {self.modalidad}")
         print(f"Jornada: {self.jornada}")
-
-#Nuevos Metodos implementados
-    def asignarDocente(self, docente):
-        if docente is None:
-            raise ValueError("Debe proporcionar un docente")
-        
-        if not isinstance(docente, Docente):
-            raise TypeError("Debe asignar un objeto Docente")
-       
-        if self.docente is not None:
-            raise Exception("El curso ya tiene un docente asignado")
-
-        self.docente = docente
-        self.historial.append(f"Docente {docente.nombre} asignado")
-
-    def buscarEstudiante(self, CedulaEstudiante):
-        if not CedulaEstudiante:
-            raise ValueError("Debe ingresar una cedula válida")
-        
-        for estudiante in self.estudiantes:
-            if estudiante.Cedula == CedulaEstudiante:
-                return estudiante 
-        return None
-    
-
-    def agregarEstudiante(self, estudiante):
-        pass
-    def retirarEstudiante(self, estudiante):
-        pass
-
-    def registrarAcccion(self, mensaje):
-        self.historial.append(mensaje)
-
-
-    def totalInscritos(self):
-        return len(self.estudiantes)
-    
-    def historialAcciones(self):
-        if self.historial:
-            return self.historial[-1]
-        return "Sin registros"
-    
-    def mostrarHistorial(self):
-        for accion in self.historial:
-            print(accion)
